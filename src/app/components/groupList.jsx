9ded1975165e969fd5915cb4c5f9f018df1a322d
Marat <C:\Users\\AppData\Roaming\The Bat!>
2022-02-14 11:10:50 +0600

    5

--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -13,21 +13,24 @@ const GroupList = ({
     // contentProperty = передается name элементов объекта
     // selectedItem = выбранная профессия
     // чтобы итерерироваться (пробежаться) по объекту, нужно  его трансформировать в массив с помощью Object.keys()
+
     return (
         <ul className="list-group">
-            {items.map((item) => (
-                <li
-                    key={item._id}
-                    className={
-                        "list-group-item " +
-                        (item === selectedItem ? " active" : "")
-                    }
-                    onClick={() => onItemSelect(item)}
-                    role="button"
-                >
-                    {item.name}
-                </li>
-            ))}
+            {Object.keys(items).map((item) => {
+                return (
+                    <li
+                        key={items[item][valueProperty]}
+                        className={
+                            "list-group-item" +
+                            (items[item] === selectedItem ? " active" : "")
+                        }
+                        onClick={() => onItemSelect(items[item])}
+                        role="button"
+                    >
+                        {items[item][contentProperty]}
+                    </li>
+                );
+            })}
         </ul>
     );
 };
