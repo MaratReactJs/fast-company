04a174d04cdbffd05b34fc3c3055abf9e03991ac
Marat <C:\Users\\AppData\Roaming\The Bat!>
2022-03-01 18:20:58 +0600

    9

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-    return <h1>Login</h1>;
+    const [data, setData] = useState({ email: "", password: "" });
+
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+    };
+
+    return (
+        <form action="">
+            <div>
+                <label htmlFor="email">
+                    {" "}
+                    Email
+                    <input
+                        type="text"
+                        id="email"
+                        value={data.email}
+                        name="email"
+                        onChange={handleChange}
+                    />
+                </label>
+            </div>
+            <div>
+                <label htmlFor="password">
+                    {" "}
+                    Password
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={data.password}
+                        onChange={handleChange}
+                    />
+                </label>
+            </div>
+        </form>
+    );
 };
 
 export default Login;
