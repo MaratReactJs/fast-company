2fd4f765dd95c1c1504b8642c259568bbe2de26d
Marat <C:\Users\\AppData\Roaming\The Bat!>
2022-03-29 12:26:04 +0600

    9

--- src/app/components/usersTable.jsx
+++ src/app/components/usersTable.jsx
@@ -5,6 +5,7 @@ import TableBody from "./tableBody";
 import BookMark from "./bookmark";
 import QualitisList from "./qualitiesList";
 import Table from "./table";
+import { Link } from "react-router-dom";
 
 const UsersTable = ({
     users,
@@ -14,7 +15,13 @@ const UsersTable = ({
     onDelete
 }) => {
     const columns = {
-        name: { path: "name", name: "Имя" },
+        name: {
+            path: "name",
+            name: "Имя",
+            component: (user) => (
+                <Link to={`users/${user._id}`}>{user.name}</Link>
+            )
+        },
         qualities: {
             name: "Качества",
             component: (user) => <QualitisList qualities={user.qualities} />
