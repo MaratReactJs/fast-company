857243ea804aa9666151fcd82d4aea39a7ba5413
Марат <D:\Program Files\The Bat!\Mail>
2022-04-20 17:41:02 +0600

    10

--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import TextField from "../common/form/textField";
-import { validator } from "../../utils/validator";
+// import { validator } from "../../utils/validator";
 import SelectField from "../common/form/selectField";
 import api from "../../api";
 import RadioField from "../common/form/radioField";
 import MultiSelectField from "../common/form/multiSelectField";
 import CheckBoxField from "../common/form/checkBoxField";
+import * as yup from "yup";
 
 const RegisterForm = () => {
     const [data, setData] = useState({
@@ -29,50 +30,50 @@ const RegisterForm = () => {
         setData((prevState) => ({ ...prevState, [target.name]: target.value }));
     };
 
-    const validatorConfig = {
-        email: {
-            isRequired: {
-                message: "Электронная почта обязательна для заполнения"
-            },
-            isEmail: {
-                message: "Email введен некорректно"
-            }
-        },
-        password: {
-            isRequired: {
-                message: "Пароль обязателен для заполнения"
-            },
-            isCapitalSymbol: {
-                message: "Пароль должен содержать хотя бы одну заглавную букву"
-            },
-            isContainDigit: {
-                message: "Пароль должен содержать хотя бы одну цифру"
-            },
-            min: {
-                message: "Пароль должен состоять минимум из 8 символов",
-                value: 8
-            }
-        },
-        profession: {
-            isRequired: {
-                message: "Обязательно выберите вашу профессию"
-            }
-        },
-        licence: {
-            isRequired: {
-                message:
-                    "Вы не можете использовать наш сервис без подтверждения лицензионного соглашения"
-            }
-        }
-    };
+    const validateSheme = yup.object().shape({
+        licence: yup
+            .boolean()
+            .oneOf(
+                [true],
+                "Вы не можете использовать наш сервис без подтверждения лицензионного соглашения"
+            ),
+        profession: yup
+            .string()
+            .required("Электронная почта обязательна для заполнения"),
+        password: yup
+            .string()
+            .required("Пароль обязателен для заполнения")
+            .matches(
+                /(?=.*[A-Z])/,
+                "Пароль должен содержать хотя бы одну заглавную букву"
+            )
+            .matches(
+                /(?=.*[0-9])/,
+                "Пароль должен содержать хотя бы одну цифру"
+            )
+            .matches(
+                /(?=.*[!@#$%^&*])/,
+                "Пароль должен содержать один из специальных символов !@#$%^&*"
+            )
+            .matches(
+                /(?=.{8,})/,
+                "Пароль должен состоять минимум из 8 символов"
+            ),
+        email: yup
+            .string()
+            .required("Электронная почта обязательна для заполнения")
+            .email("Email введен некорректно")
+    });
 
     useEffect(() => {
         validate();
     }, [data]);
 
     const validate = () => {
-        const errors = validator(data, validatorConfig);
-        setErrors(errors);
+        validateSheme
+            .validate(data)
+            .then(() => setErrors({}))
+            .catch((err) => setErrors({ [err.path]: err.message }));
         return Object.keys(errors).length === 0;
     };
 
@@ -109,7 +110,7 @@ const RegisterForm = () => {
                 options={professions}
                 defaultOption="Choose...."
                 error={errors.profession}
-                name="professions"
+                name="profession"
             />
 
             <RadioField
